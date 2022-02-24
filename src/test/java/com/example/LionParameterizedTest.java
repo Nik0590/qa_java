8208c700e7cedd83fd597d7407138b179e0f1c75
Nikita Tikhonov <TikhonovNikitaIgorevic@yandex.ru>
2022-02-24 12:46:43 +0300

    add LionParameterizedTest

---
+++ src/test/java/com/example/LionParameterizedTest.java
@@ -0,0 +1,40 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+
+
+public class LionParameterizedTest {
+
+    // поля класса:
+    private final String checkedSex; // текст, который передаётся в метод
+    private final Boolean expected; // ожидаемый результат
+
+    // конструктор с параметрами
+    public LionParameterizedTest(String checkedSex, Boolean expected) {
+        this.checkedSex = checkedSex;
+        this.expected = expected;
+    }
+
+    // метод для получения данных
+    @Parameterized.Parameters
+    public static Object[][] getSexData() {
+        return new Object[][]{
+                {"Самец", true},
+                {"Самка", false},
+        };
+    }
+
+    @Test
+    public void shouldDoesHaveMane() throws Exception {
+        Lion lion = new Lion(checkedSex);
+        boolean actual = lion.doesHaveMane();
+        assertEquals(expected, actual);
+    }
+}
