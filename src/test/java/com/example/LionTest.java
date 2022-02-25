23bdacd639ba580acae5b1f285a6d31bb2ecad36
Nikita Tikhonov <TikhonovNikitaIgorevic@yandex.ru>
2022-02-25 12:27:34 +0300

    Sprint_2_v3

--- src/test/java/com/example/LionTest.java
+++ src/test/java/com/example/LionTest.java
@@ -4,47 +4,47 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import java.util.List;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
-
-import static org.junit.Assert.assertEquals;
+import org.junit.runner.RunWith;
 
 @RunWith(MockitoJUnitRunner.class)
+public class LionTest {
 
-    public class LionTest {
+    @Mock
+    Feline feline;
 
-        @Mock
-        Feline feline;
+    @Rule
 
-        @Rule
-        public ExpectedException exeptionRule = ExpectedException.none();
-        @Test
-        public void shouldGetKittens() throws Exception {
-            Lion lion = new Lion("Самка");
-            int actual = lion.getKittens(1);
-            int expected = 1;
-            assertEquals(expected, actual);
-        }
+    public ExpectedException exeptionRule = ExpectedException.none();
 
-        @Test
-        public void shouldGetFood() throws Exception {
-            Lion lion = new Lion(feline);
-            Mockito.when(lion.getFood()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
-            List<String> actual = lion.getFood();
-            List<String> expected = List.of("Животные", "Птицы", "Рыба");
-            assertEquals(expected, actual);
-        }
+    @Test
+    public void shouldGetKittens() throws Exception {
+        int kittensCount = 1;
+        Lion lion = new Lion(feline);
+        int actual = lion.getKittens(kittensCount);
+        int expected = 1;
+        assertEquals(expected, actual);
+    }
 
-        @Test
-        public void checkExceptionMessage() throws Exception {
-            exeptionRule.expect(Exception.class);
-            exeptionRule.expectMessage("Используйте допустимые значения пола животного - самец или самка");
-            Lion lion = new Lion("sdf");
-            boolean expected = true;
-            boolean actual = lion.doesHaveMane();
-            assertEquals(expected, actual);
-        }
+    @Test
+    public void shouldGetFood() throws Exception{
+        Lion lion = new Lion(feline);
+        Mockito.when(lion.getFood()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+        List<String> actual = lion.getFood();
+        List<String> expected = List.of("Животные", "Птицы", "Рыба");
+        assertEquals(expected, actual);
+    }
 
+    @Test
+    public void doesNotHaveMane() throws Exception {
+        exeptionRule.expect(Exception.class);
+        exeptionRule.expectMessage("Используйте допустимые значения пола животного - самец или самка");
+        Lion lion = new Lion("sdf");
+        boolean expected = true;
+        boolean actual = lion.doesHaveMane();
+        assertEquals(expected, actual);
+    }
 }
